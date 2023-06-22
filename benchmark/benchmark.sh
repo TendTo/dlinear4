2f2353f434a329e1d86e2edaeea0792350d366f1
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-06-22 19:32:38 +0200

    feat: include soplex in benchmark script

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,9 +1,7 @@
-json_file="/app/benchmark/benchmark.json"
-csv_file="/app/benchmark/benchmark.csv"
+#!/bin/bash
+json_file="/home/ubuntu/dlinear4/benchmark/benchmark.json"
+csv_file="/home/ubuntu/dlinear4/benchmark/benchmark.csv"
 
-BENCHMARK_OUT="${json_file}" BENCHMARK_OUT_FORMAT=json bazel run //benchmark
-# When soplex support is added
-# BENCHMARK_OUT="${json_file}" BENCHMARK_OUT_FORMAT=json bazel run //benchmark --//:enable-soplex=true
-# BENCHMARK_OUT="/app/benchmarks.txt" BENCHMARK_OUT_FORMAT=csv bazel run //benchmark --//:enable-soplex=true --cxxopt=-fext-numeric-literals --define SOPLEX_WITH_BOOST
+BENCHMARK_OUT="${json_file}" BENCHMARK_OUT_FORMAT=json bazel run //benchmark --//:enable-soplex=true
 
 ./benchmark_parser.py "${json_file}" "${csv_file}"
